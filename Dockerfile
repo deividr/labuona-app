e817562ddd0797760fafd78ff18efc68f3941c73
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2023-11-20 10:35:55 -0300

    chore: update all libs

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18-slim
+FROM node:20-slim
 
 RUN mkdir -p /home/node/app
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '../app.css';
 </script>
 
+<nav class="relative">
+	<a href="/">Home</a>
+	<a href="/login">Login</a>
+</nav>
+
 <slot />
